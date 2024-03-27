e7682db5b8f046a201147b90c07cef1eba591633
Kelly Anthis <10109964+persever@users.noreply.github.com>
2024-03-27 12:40:13 -0400

    add get route and tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -50,6 +50,7 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
+
     ######################################################################
     #  H E L P E R   M E T H O D S
     ######################################################################
@@ -123,4 +124,14 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should read an account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.get(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(account.serialize(), response.get_json())
+
+    def test_account_not_found(self):
+        """It should return a 404 if the requested account is not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
